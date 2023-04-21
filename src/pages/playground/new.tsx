f00b8969df7420ce8b49501e8fc5c60aea77ebb2
손정만 <sonjm1758@gmail.com>
2023-04-21 21:46:25 +0900

    eslint 설정

--- src/pages/playground/new.tsx
+++ src/pages/playground/new.tsx
@@ -1,9 +1,9 @@
-import { ParsedUrlQueryInput } from 'node:querystring';
+import { ParsedUrlQueryInput } from 'node:querystring'
 
-import { useRouter } from 'next/router';
-import { useState } from 'react';
+import { useRouter } from 'next/router'
+import { useState } from 'react'
 
-import BaseLayout from '../../components/layouts/baseLayout';
+import BaseLayout from '../../components/layouts/baseLayout'
 
 interface Query extends ParsedUrlQueryInput{
   words?: string,
@@ -12,30 +12,30 @@ interface Query extends ParsedUrlQueryInput{
 }
 
 export default function Home() {
-  const router = useRouter();
+  const router = useRouter()
 
-  const [words, setWords] = useState('');
-  const [memberCount, setMemberCount] = useState(2);
-  const [startNumber, setStartNumber] = useState(1);
+  const [words, setWords] = useState('')
+  const [memberCount, setMemberCount] = useState(2)
+  const [startNumber, setStartNumber] = useState(1)
 
   const generateNewGame = () => {
-    alert('게임을 생성합니다');
-    const query: Query = {};
+    alert('게임을 생성합니다')
+    const query: Query = {}
     if (words?.length){
-      query.words = words;
+      query.words = words
     }
     if (memberCount >= 2){
-      query.memberCount = memberCount;
+      query.memberCount = memberCount
     }
     if (startNumber >= 1){
-      query.startNumber = startNumber;
+      query.startNumber = startNumber
     }
 
     router.push({
       pathname: '/playground/game',
       query,
-    });
-  };
+    })
+  }
 
   return (
     <BaseLayout title='호구마 게임'>
@@ -59,5 +59,5 @@ export default function Home() {
         생성
       </button>
     </BaseLayout>
-  );
+  )
 }
