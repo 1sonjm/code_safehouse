09c69f782a6d1a18da7fbdecde1bcd20d880655b
손정만 <sonjm1758@gmail.com>
2023-04-30 00:57:51 +0900

    eslint 설정 변경 및 규칙 적용

--- src/lib/HoGuMaMachine.ts
+++ src/lib/HoGuMaMachine.ts
@@ -16,213 +16,213 @@ export interface GameStep {
 }
 
 export default class HoGuMaMachine {
-  date:Date = new Date()
-  history: Array<GameHistory> = []
+	date:Date = new Date()
+	history: Array<GameHistory> = []
 
-  private gameSetting: GameSetting = {
-    words: '',
-    memberCount: 2,
-    startIndex: 0,
-  }
-  rearrangedWords!:Array<string>
-  wordsIndex:number = 0
-  count:number = 0
-  isCountUserOnly:boolean = false
+	private gameSetting: GameSetting = {
+		words: '',
+		memberCount: 2,
+		startIndex: 0,
+	}
+	rearrangedWords!:Array<string>
+	wordsIndex:number = 0
+	count:number = 0
+	isCountUserOnly:boolean = false
 
-  constructor(words?:string, memberCount?:number, startIndex?: number) {
-    this.updateGameSetting(words, memberCount, startIndex)
+	constructor(words?:string, memberCount?:number, startIndex?: number) {
+		this.updateGameSetting(words, memberCount, startIndex)
 
-    this.rearrangeWordsProcess()
-  }
+		this.rearrangeWordsProcess()
+	}
 
-  /**
+	/**
    * 게임 설정값 오류 확인
    */
-  private updateGameSetting (
-    words: string | null = this.gameSetting.words || '호박고구마',
-    memberCount: number | null = this.gameSetting.memberCount || 2,
-    startNumber: number | null = this.gameSetting.startIndex || 1
-  ) {
-    this.reset()
+	private updateGameSetting (
+		words: string | null = this.gameSetting.words || '호박고구마',
+		memberCount: number | null = this.gameSetting.memberCount || 2,
+		startNumber: number | null = this.gameSetting.startIndex || 1
+	) {
+		this.reset()
 
-    if (words !== null) {
-      if (words.length) {
-        this.gameSetting.words = words
-      } else {
-        throw new Error(`제시 단어가 잘못되었습니다. 입력값: ${words}`)
-      }
-    }
+		if (words !== null) {
+			if (words.length) {
+				this.gameSetting.words = words
+			} else {
+				throw new Error(`제시 단어가 잘못되었습니다. 입력값: ${words}`)
+			}
+		}
 
-    if(memberCount !== null){
-      if (memberCount > 1) {
-        this.gameSetting.memberCount = memberCount
-      } else {
-        throw new Error(`참가인원이 잘못되었습니다. 입력값: ${memberCount}`)
-      }
-    }
+		if(memberCount !== null){
+			if (memberCount > 1) {
+				this.gameSetting.memberCount = memberCount
+			} else {
+				throw new Error(`참가인원이 잘못되었습니다. 입력값: ${memberCount}`)
+			}
+		}
 
-    if(startNumber !== null){
-      if (startNumber >= 0 && startNumber <= this.gameSetting.memberCount) {
-        this.gameSetting.startIndex = startNumber - 1
-      } else {
-        throw new Error(`순번이 잘못되었습니다. 입력값: ${startNumber}`)
-      }
-    }
-  }
+		if(startNumber !== null){
+			if (startNumber >= 0 && startNumber <= this.gameSetting.memberCount) {
+				this.gameSetting.startIndex = startNumber - 1
+			} else {
+				throw new Error(`순번이 잘못되었습니다. 입력값: ${startNumber}`)
+			}
+		}
+	}
 
-  /**
+	/**
    * 제시 단어를 재정렬하여 초기화
    */
-  private rearrangeWordsProcess () {
-    const { words, memberCount, startIndex } = this.gameSetting
-    const rangeLimit = words.length > memberCount ? words.length : memberCount
-    let i = startIndex
+	private rearrangeWordsProcess () {
+		const { words, memberCount, startIndex } = this.gameSetting
+		const rangeLimit = words.length > memberCount ? words.length : memberCount
+		let i = startIndex
 
-    this.rearrangedWords = []
-    while (this.rearrangedWords.length < rangeLimit) {
-      while (i >= words.length) {
-        i -= words.length
-      }
+		this.rearrangedWords = []
+		while (this.rearrangedWords.length < rangeLimit) {
+			while (i >= words.length) {
+				i -= words.length
+			}
 
-      this.rearrangedWords.push(words[i])
-      if (this.rearrangedWords.length >= words.length) {
-        const repeat = this.findRepeatingWords(this.rearrangedWords)
-        if (repeat) {
-          break
-        }
-      }
+			this.rearrangedWords.push(words[i])
+			if (this.rearrangedWords.length >= words.length) {
+				const repeat = this.findRepeatingWords(this.rearrangedWords)
+				if (repeat) {
+					break
+				}
+			}
 
-      i += memberCount
-    }
+			i += memberCount
+		}
 
-    this.saveHistory()
-  }
+		this.saveHistory()
+	}
 
-  /**
+	/**
    * 반복되는 단어 추출
    * @returns 재정렬된 단어
    */
-  private findRepeatingWords(targetWords: string[]): string[] | null {
-    for (let i = 0; i < targetWords.length; i++) {
-      let checkedWords = targetWords.slice(0, i + 1)
-      let repeated = true
-      for (let j = 0; j < targetWords.length; j += checkedWords.length) {
-        if (targetWords.slice(j, j + checkedWords.length).join('') !== checkedWords.join('')) {
-          repeated = false
-          break
-        }
-      }
-      if (repeated) {
-        return checkedWords
-      }
-    }
-    return null
-  }
+	private findRepeatingWords(targetWords: string[]): string[] | null {
+		for (let i = 0; i < targetWords.length; i++) {
+			let checkedWords = targetWords.slice(0, i + 1)
+			let repeated = true
+			for (let j = 0; j < targetWords.length; j += checkedWords.length) {
+				if (targetWords.slice(j, j + checkedWords.length).join('') !== checkedWords.join('')) {
+					repeated = false
+					break
+				}
+			}
+			if (repeated) {
+				return checkedWords
+			}
+		}
+		return null
+	}
 
-  /**
+	/**
    * 지난 게임정보 기록
    */
-  private saveHistory() {
-    if (this.history.length > 10) {
-      this.history.splice(0, 1)
-    }
+	private saveHistory() {
+		if (this.history.length > 10) {
+			this.history.splice(0, 1)
+		}
 
-    this.history.push({
-      gameSetting: {...this.gameSetting},
-      rearrangedWords: [...this.rearrangedWords],
-    })
-  }
-  public getHistory() {
-    return this.history
-  }
+		this.history.push({
+			gameSetting: {...this.gameSetting},
+			rearrangedWords: [...this.rearrangedWords],
+		})
+	}
+	public getHistory() {
+		return this.history
+	}
 
-  /**
+	/**
    * 참여 순번 변경
    * @param newStartIndex 변경할 순번
    */
-  public changeStartIndex (newStartIndex:number) {
-    this.updateGameSetting(null, null, newStartIndex)
-    this.rearrangeWordsProcess()
-  }
+	public changeStartIndex (newStartIndex:number) {
+		this.updateGameSetting(null, null, newStartIndex)
+		this.rearrangeWordsProcess()
+	}
 
-  /**
+	/**
    * 참여자 수 변경
    * @param newMemberCount 변경할 인원
    * @param newStartIndex 변경할 순번
    */
-  public changeMemberCount (newMemberCount:number, newStartIndex?:number | null) {
-    this.updateGameSetting(null, newMemberCount, newStartIndex)
-    this.rearrangeWordsProcess()
-  }
+	public changeMemberCount (newMemberCount:number, newStartIndex?:number | null) {
+		this.updateGameSetting(null, newMemberCount, newStartIndex)
+		this.rearrangeWordsProcess()
+	}
 
-  /**
+	/**
    * 제시 단어 변경
    * @param newStartIndex 변경할 순번
    * @param newMemberCount 변경할 인원
    * @param newStartIndex 변경할 순번
    */
-  public changeWords (newWords:string, newMemberCount?:number | null, newStartIndex?:number | null) {
-    this.updateGameSetting(newWords, newMemberCount, newStartIndex)
-    this.rearrangeWordsProcess()
-  }
+	public changeWords (newWords:string, newMemberCount?:number | null, newStartIndex?:number | null) {
+		this.updateGameSetting(newWords, newMemberCount, newStartIndex)
+		this.rearrangeWordsProcess()
+	}
 
-  /**
+	/**
    * 재정렬된 제시어를 반환
    * @returns 재정렬된 단어
    */
-  public getRearrangeWords () {
-    return {
-      gameSetting: this.gameSetting,
-      rearrangedWords: this.rearrangedWords,
-    }
-  }
+	public getRearrangeWords () {
+		return {
+			gameSetting: this.gameSetting,
+			rearrangedWords: this.rearrangedWords,
+		}
+	}
 
-  /**
+	/**
    * 진행상황 초기화
    */
-  public reset() {
-    this.wordsIndex = 0
-    this.count = 0
-  }
+	public reset() {
+		this.wordsIndex = 0
+		this.count = 0
+	}
 
-  /**
+	/**
    * 순번대로 제시어에 대한 결과를 반환
    * @returns 순번 진행 결과
    */
-  public async nextStep (): Promise<GameStep> {
-    return new Promise((resolve, rejects) => {
-      let repeatCount = this.isCountUserOnly ? 2 : 1
+	public async nextStep (): Promise<GameStep> {
+		return new Promise((resolve, rejects) => {
+			let repeatCount = this.isCountUserOnly ? 2 : 1
 
-      while (repeatCount > 0) {
-        repeatCount -= 1
-        this.count += 1
+			while (repeatCount > 0) {
+				repeatCount -= 1
+				this.count += 1
 
-        const step = this.getStep()
+				const step = this.getStep()
 
-        this.wordsIndex += 1
-        if (this.wordsIndex > this.gameSetting.words.length - 1){
-          this.wordsIndex = 0
-        }
+				this.wordsIndex += 1
+				if (this.wordsIndex > this.gameSetting.words.length - 1){
+					this.wordsIndex = 0
+				}
 
-        resolve(step)
-      }
-      rejects(null)
-    })
-  }
+				resolve(step)
+			}
+			rejects(null)
+		})
+	}
 
-  public getStep () {
-    const step:GameStep = {
-      char: this.gameSetting.words[this.wordsIndex],
-      isMyTurn: false,
-      totalCount: this.count,
-    }
+	public getStep () {
+		const step:GameStep = {
+			char: this.gameSetting.words[this.wordsIndex],
+			isMyTurn: false,
+			totalCount: this.count,
+		}
 
-    if (this.count % this.gameSetting.memberCount - 1 === 0) {
-      step.isMyTurn = true
-    }
+		if (this.count % this.gameSetting.memberCount - 1 === 0) {
+			step.isMyTurn = true
+		}
 
-    return step
-  }
+		return step
+	}
 }
 
 /**
--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,3 +1,3 @@
-export default (req, res) => {
-  res.status(200).json({ text: 'Hello' })
-}
+// export default hello(req, res){
+// 	res.status(200).json({ text: 'Hello' })
+// }
